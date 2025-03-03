452b089e649dd8086b235b9c2457a3c098637b61
niharika464 <niharikasinghgaur11@gmail.com>
2025-03-03 21:28:07 +0530

    class student

---
+++ class.py
@@ -0,0 +1,15 @@
+class student:
+    grade=12
+    name="Niharika"
+
+    def intro(self):
+        print("Hello, Nice to meet you....")
+
+    def details(self):
+         print("my name is:",self.name)
+         print("my grade is:",self.grade)
+
+            
+s=student()
+s.intro()
+s.details()            
